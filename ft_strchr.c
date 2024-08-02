55e9489c5d93bd4bfd3bbef4b35d291299da23df
Imane Qandyl <imqandyl@lab1r2s10.42abudhabi.ae>
2024-08-02 13:33:58 +0400

    update

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: imqandyl <imqandyl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 22:44:11 by imqandyl          #+#    #+#             */
-/*   Updated: 2024/07/31 23:09:49 by imqandyl         ###   ########.fr       */
+/*   Updated: 2024/08/02 13:28:23 by imqandyl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0')
+	size_t	i;
+
+	i = 0;
+	while (s[i])
 	{
-		if (*s == (char)c)
-		{
-			return ((char *)s);
-		}
-		s++;
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+	{
+		return ((char *)&s[i]);
 	}
 	return (NULL);
 }
